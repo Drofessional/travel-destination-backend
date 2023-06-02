4fac4b0fa14f4f24673b8242f79d809f8c97dcc7
mikefess <62866256+Drofessional@users.noreply.github.com>
2023-06-02 17:19:16 -0400

    Fixed issue w/ routes

--- server/controllers/controllers.js
+++ server/controllers/controllers.js
@@ -57,7 +57,6 @@ exports.addDestination = async (req, res) => {
   res.json(user.destinations);
 };
 
-
 exports.updateDestination = async (req, res) => {
   const user = await User.findOne({ name: req.params.name });
   if (!user) return res.status(404).json({ message: 'User not found' });
