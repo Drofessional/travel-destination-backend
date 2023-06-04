9dcea2c4df9203c502359a185bc7378c5b2029c7
mikefess <62866256+Drofessional@users.noreply.github.com>
2023-06-04 14:19:13 -0400

    plz

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ const mongoose = require('./db/connection');
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors());
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
 //   app.use(cors(corsOptions));
