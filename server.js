b22d6237c7581c998fbfbf555f7fe571b68652bd
mikefess <62866256+Drofessional@users.noreply.github.com>
2023-06-04 14:06:54 -0400

    Trying to fix CORS

--- server.js
+++ server.js
@@ -7,7 +7,16 @@ const mongoose = require('./db/connection');
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.use(cors());
+// app.use(cors(corsOptions));
+const corsOptions = {
+    origin: 'http://127.0.0.1:5500', // Allow requests from a specific origin (replace with your domain)
+    methods: ['GET', 'POST'], // Allow specific HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow specific headers
+    credentials: true, // Enable sending cookies with requests (for authenticated requests)
+    optionsSuccessStatus: 200 // Return 200 for successful preflight requests
+  };
+  
+  app.use(cors(corsOptions));
 app.use(express.json());
 console.log(process.env.JWT_SECRET);
 
